b238ca52e7a5a30f1ea556454456372f98dd788f
nbrasset <nicolas.brasset@etu.univ-paris-diderot.fr>
2019-07-22 11:06:21 +0200

    added some conditions for generating triplets and pushing .wavs

--- experiments/pilot_May_2019/triplet_creation/generate_triplets.py
+++ experiments/pilot_May_2019/triplet_creation/generate_triplets.py
@@ -21,7 +21,16 @@ soundsdf.columns = ["file","onset","offset","item","word","speaker","CV","contex
 output_file = open(args.output_path,"w")
 output_file.write("file_TGT,onset_TGT,offset_TGT,item_TGT,word_TGT,speaker_TGT,CV_TGT,context_TGT,phone_TGT,file_OTH,onset_OTH,offset_OTH,item_OTH,word_OTH,speaker_OTH,CV_OTH,context_OTH,phone_OTH,file_X,onset_X,offset_X,item_X,word_X,speaker_X,CV_X,context_X,phone_X\n")
 for TGT in soundsdf.index :
+	TGT_speaker = soundsdf.get_value(TGT,"speaker")
 	for OTH in soundsdf.index :
-		for X in soundsdf.index :
-			output_file.write(str(soundsdf.get_value(TGT,"file")) + ',' + str(soundsdf.get_value(TGT,"onset")) + ',' + str(soundsdf.get_value(TGT,"offset")) + ',' + str(soundsdf.get_value(TGT,"item")) + ',' + str(soundsdf.get_value(TGT,"word")) + ',' + str(soundsdf.get_value(TGT,"speaker")) + ',' + str(soundsdf.get_value(TGT,"CV")) + ',' + str(soundsdf.get_value(TGT,"context")) + ',' + str(soundsdf.get_value(TGT,"phone")) + ',' + str(soundsdf.get_value(OTH,"file")) + ',' + str(soundsdf.get_value(OTH,"onset")) + ',' + str(soundsdf.get_value(OTH,"offset")) + ',' + str(soundsdf.get_value(OTH,"item")) + ',' + str(soundsdf.get_value(OTH,"word")) + ',' + str(soundsdf.get_value(OTH,"speaker")) + ',' + str(soundsdf.get_value(OTH,"CV")) + ',' + str(soundsdf.get_value(OTH,"context")) + ',' + str(soundsdf.get_value(OTH,"phone")) + ',' + str(soundsdf.get_value(X,"file")) + ',' + str(soundsdf.get_value(X,"onset")) + ','+ str(soundsdf.get_value(X,"offset")) + ',' + str(soundsdf.get_value(X,"item")) + ',' + str(soundsdf.get_value(X,"word")) + ',' + str(soundsdf.get_value(X,"speaker")) + ','+ str(soundsdf.get_value(X,"CV")) + ',' + str(soundsdf.get_value(X,"context")) + ',' + str(soundsdf.get_value(X,"phone")) + '\n')
+		OTH_speaker = soundsdf.get_value(OTH,"speaker")
+		if (TGT_speaker[-1] != OTH_speaker[-1]):
+			for X in soundsdf.index :
+				X_speaker = soundsdf.get_value(X,"speaker")
+				if (TGT_speaker[:-1] != X_speaker[:-1]) and (OTH_speaker[:-1] != X_speaker[:-1]) and (OTH_speaker[:-1] != TGT_speaker[:-1]):
+					TGT_phone = soundsdf.get_value(TGT,"phone")
+					OTH_phone = soundsdf.get_value(OTH,"phone")
+					X_phone = soundsdf.get_value(X,"phone")
+					if (TGT_phone != OTH_phone) and ((TGT_phone == X_phone) or (OTH_phone == X_phone)): 
+						output_file.write(str(soundsdf.get_value(TGT,"file")) + ',' + str(soundsdf.get_value(TGT,"onset")) + ',' + str(soundsdf.get_value(TGT,"offset")) + ',' + str(soundsdf.get_value(TGT,"item")) + ',' + str(soundsdf.get_value(TGT,"word")) + ',' + str(soundsdf.get_value(TGT,"speaker")) + ',' + str(soundsdf.get_value(TGT,"CV")) + ',' + str(soundsdf.get_value(TGT,"context")) + ',' + str(soundsdf.get_value(TGT,"phone")) + ',' + str(soundsdf.get_value(OTH,"file")) + ',' + str(soundsdf.get_value(OTH,"onset")) + ',' + str(soundsdf.get_value(OTH,"offset")) + ',' + str(soundsdf.get_value(OTH,"item")) + ',' + str(soundsdf.get_value(OTH,"word")) + ',' + str(soundsdf.get_value(OTH,"speaker")) + ',' + str(soundsdf.get_value(OTH,"CV")) + ',' + str(soundsdf.get_value(OTH,"context")) + ',' + str(soundsdf.get_value(OTH,"phone")) + ',' + str(soundsdf.get_value(X,"file")) + ',' + str(soundsdf.get_value(X,"onset")) + ','+ str(soundsdf.get_value(X,"offset")) + ',' + str(soundsdf.get_value(X,"item")) + ',' + str(soundsdf.get_value(X,"word")) + ',' + str(soundsdf.get_value(X,"speaker")) + ','+ str(soundsdf.get_value(X,"CV")) + ',' + str(soundsdf.get_value(X,"context")) + ',' + str(soundsdf.get_value(X,"phone")) + '\n')
 output_file.close()
