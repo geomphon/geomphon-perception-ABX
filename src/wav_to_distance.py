4d6f5d6af56449309c8eda701dc0af84d54dd55b
Ewan Dunbar <emd@umd.edu>
2019-08-23 20:11:54 +0200

    acoustic distance script functional

--- src/wav_to_distance.py
+++ src/wav_to_distance.py
@@ -6,6 +6,7 @@ import os.path as osp
 
 import numpy as np
 import pandas as pd
+import scipy
 
 import shennong.features.pipeline as snpipeline
 from fastdtw import fastdtw
@@ -15,109 +16,107 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)  # FIXME WRAP
 
 
-
-# FIXME: It is broke!
-
-#def dtw_euclidean(x, y):
-#    return fastdtw(x, y, dist=2)[0]
-
-#def dtw_norm_max_euclidean(x, y):
-#    dtw = fastdtw(x, y, dist=2)
-#    return dtw[0] / len(dtw[1])
-
-#def dtw_norm_both_euclidean(x, y):
-#    dtw = fastdtw(x, y, dist=2)
-#    return dtw[0] / (x.shape[0] + y.shape[0])
-
-#def get_item_1(df, row):
-#    info = pd.DataFrame([row[[c for c in row.keys() \
-#            if len(c) > 2 and c[-2:] == "_1"]]])
-#    info.columns = [c[:-2] for c in info.columns]
-#    item = info.merge(df, how='left')
-#    return item[[c for c in item.columns if c[0] == "F"]]
-#
-#
-#def get_item_2(df, row):
-#    info = pd.DataFrame([row[[c for c in row.keys() \
-#                            if len(c) > 2 and c[-2:] == "_2"]]])
-#    info.columns = [c[:-2] for c in info.columns]
-#    item = info.merge(df, how='left')
-#    return item[[c for c in item.columns if c[0] == "F"]]
+def cosine_distance(x, y):
+    '''[summary]
+    
+    :param x: [description]
+    :type x: [type]
+    :param y: [description]
+    :type y: [type]
+    '''
+    xnorm = np.sqrt(np.sum(x**2))
+    ynorm = np.sqrt(np.sum(y**2))
+    cos = np.dot(x, y) / (xnorm * ynorm)
+    return scipy.arccos(cos) / np.pi
 
 
-def center_time(frame_numbers, frame_shift):
-    return frame_numbers * frame_shift
+def dtw_cosine_mean(x, y):
+    '''[summary]
+    
+    :param x: [description]
+    :type x: [type]
+    :param y: [description]
+    :type y: [type]
+    :return: [description]
+    :rtype: [type]
+    '''
+    dtw = fastdtw(x, y, dist=cosine_distance)
+    return dtw[0] / len(dtw[1])
 
 
-#def replace_content(d, new_content):
-#    result = d.copy()
-#    result[sf.content_keys(d)] = new_content
-#    return result
-
-#        features_ = psf.mfcc(signal, wav_sampling_rate, feature_window_length,
-#                             feature_frame_shift, nceps, nfilt, 512, 133.3333,
-#                             6855.4976, 0.97, 0, True, np.hamming)
-#        features_ = psf.logfbank(signal, wav_sampling_rate,
-#                                 feature_window_length, feature_frame_shift,
-#                                 nfilt, 512, 133.3333, 6855.4976, 0.97,
-#                                 np.hamming)
-
-#        columns=["F" + str(i) for i in range(1, features_.shape[1] + 1)])
-#    # FIXME - use the (non-existent) standard_format API to set meta-columns
-#    features_df['_frame_number'] = range(features_.shape[0])
-#    # FIXME - this is safe to do independent of the treatment ONLY
-#    # because stft used zero-padding; if it had truncated, we would have
-#    # needed the feature function to give us back the times
-#    features_df['_time'] = center_time(features_df['_frame_number'],
-#                                       feature_frame_shift)
-#    features_df['_file'] = filename
-
-def get_item(features, filename, onset, offset):
-   '''Get features corresponding to a chunk of a file
-   
-   :param features: Dictionary of features keyed by filename
-   :type features: [type]
-   :param filename: [description]
-   :type filename: [type]
-   :param onset: [description]
-   :type onset: [type]
-   :param offset: [description]
-   :type offset: [type]
-   :return: [description]
-   :rtype: [type]
-   ''' 
-    if 'onset' in row.index and 'offset' in row.index \
-       and 'time' in df.columns:
-        rep = df[(df['file'] == row['file']) \
-                 & (df['time'] >= row['onset'] - frame_tolerance) \
-                 & (df['time'] <= row['offset'] + frame_tolerance)]
-    else:
-        rep = df[df['file'] == row['file']]
-    meta = pd.DataFrame([row])
-    result = rep.merge(meta, on='file', how='left')
-    result['item_frame'] = range(rep.shape[0])
+def get_midpoints(features):
+    '''Retrieve frame midpoint times from Shennong Features object,
+    calculating and caching them in the feature object if they don't exist.
+    
+    :param features: Shennong features object
+    :type features: shennong.features.features.Features
+    '''
+    try:
+        result = features.midpoints
+    except AttributeError:
+        features.midpoints = (features.times[:, 0] + features.times[:, 1]) / 2.
+        result = features.midpoints
     return result
 
 
-def calculate_distances(pairs, features, distance_fn):
+def nearest_frame(time, midpoints):
+    '''Returns index of frame with the nearest midpoint to a given time.
+    
+    :param time: Time to find
+    :type time: float
+    :param midpoints: Frame midpoints
+    :type midpoints: ndarray
+    '''
+    return np.argmin(np.abs(midpoints - time))
+
+
+def get_item(features, utterance_id, onset, offset):
+    '''Get features corresponding to a chunk of a file
+   
+    :param features: Dictionary of features
+    :type features: dict 
+    :param utterance_id:  Utterance ID (key into feature dictionary)
+    :type filename: str 
+    :param onset: Onset time (seconds)
+    :type onset: float 
+    :param offset: Offset time (seconds)
+    :type offset: float
+    :return: Features corresponding to the requested chunk
+    :rtype: ndarray
+    '''
+    midpoints = get_midpoints(features[utterance_id])
+    frame_onset = nearest_frame(onset, midpoints)
+    frame_offset = nearest_frame(offset, midpoints)
+    return features[utterance_id].data[frame_onset:frame_offset, ]  # FIXME: 0?
+
+
+def calculate_distances(pairs,
+                        features,
+                        utterance_ids,
+                        distance_fn=dtw_cosine_mean):
     '''Calculate distances between given pairs.
     
     :param pairs: Pairs to be calculated
     :type pairs: Pandas DataFrame
     :param features: A dictionary of Shennong Features objects, indexed by
-    filename
+    (filename, speaker) tuples
     :type features: dict
+    :param utterance_ids: A dictionary mapping (file, speaker) pairs to
+    utterance_ids
+    :type utterance_ids: dict
     :param distance_fn: A distance function applying to pairs of ndarrays
     :type distance_fn: function
     :return: A list of distance values
     :rtype: list
     '''
     return [distance_fn(get_item(features,
-                                 pairs.iloc[i].loc['file_1'],
+                                 utterance_ids[(pairs.iloc[i].loc['file_1'],
+                                  pairs.iloc[i].loc['speaker_1'])],
                                  pairs.iloc[i].loc['onset_1'],
                                  pairs.iloc[i].loc['offset_1']),
                         get_item(features,
-                                 pairs.iloc[i].loc['file_2'],
+                                 utterance_ids[(pairs.iloc[i].loc['file_2'],
+                                  pairs.iloc[i].loc['speaker_2'])],
                                  pairs.iloc[i].loc['onset_2'],
                                  pairs.iloc[i].loc['offset_2'])) \
             for i in range(len(pairs))]
@@ -127,9 +126,6 @@ def BUILD_ARGPARSE():
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    #    parser.add_argument('--distance-function', help="Name of a distance "\
-    #            "function (default is 'dtw_norm_max_euclidean')", type=str,
-    #            default="dtw_norm_max_euclidean")
     parser.add_argument('pair_file', help="Pair file", type=str)
     parser.add_argument('shennong_config_file',
                         help="Shennong feature extraction config file",
@@ -165,24 +161,21 @@ if __name__ == "__main__":
         eprint("""Issue with pair file (<F>):
 columns don't match""".replace("<F>", str(args.pair_file)))
         sys.exit(1)
-    if not set(['file', 'onset', 'offset']).issubset(items_1.columns):
-        eprint("""Issue with pair file (<F>): missing 'file', 'onset',
-and 'offset' columns""".replace("<F>", str(args.pair_file)))
+    if not set(['file', 'onset', 'offset', 'speaker']).issubset(
+            items_1.columns):
+        eprint("""Issue with pair file (<F>): missing 'file', 'speaker',
+'onset', and 'offset' columns""".replace("<F>", str(args.pair_file)))
         sys.exit(1)
     items = pd.concat([items_1, items_2], sort=True).drop_duplicates()
-    files = items['file'].unique()
+    file_spk_ = items[['file', 'speaker']].drop_duplicates()
+    utterance_index = [(str(i), ) + tuple(x)
+                       for (i, x) in enumerate(file_spk_.values)]
+    utterance_ids = dict(((f, s), uid) for (uid, f, s) in utterance_index)
 
-    #    try:
-    #        distance_fn = globals()[args.distance_function]
-    #    except Exception as e:
-    #        eprint("""Issue with distance function (<F>): <M>""".replace(
-    #            "<F>",
-    #            str(args.distance_function)).replace("<M>",
-    #                                                 str(e)).replace("\n", " "))
-    #        sys.exit(1)
-
-    features = snpipeline.extract_features(args.shennong_config_file, files)
-    distances = calculate_distances(pairs, features, distance_fn)
+    features = snpipeline.extract_features(args.shennong_config_file,
+                                           utterance_index,
+                                           njobs=3)
+    distances = calculate_distances(pairs, features, utterance_ids)
     pairs['distance'] = distances
 
     if args.output_file is None:
