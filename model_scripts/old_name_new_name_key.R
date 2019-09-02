327801e66d43de91c14e417c3d2fbd5efea916f3
ameliakimball <amelia.kimball@gmail.com>
2019-09-02 14:38:33 +0200

    updates script which maps old .rds names to new .rds names

--- model_scripts/old_name_new_name_key.R
+++ model_scripts/old_name_new_name_key.R
@@ -1,24 +1,38 @@
 #rename old files to new names to allow for loo comparisons 
-
 #create master df based on new rules 
+
+#should be in directory model_scripts
+
 `%>%`<-magrittr::`%>%`
 EXPERIMENT_NAME<- "hk"
 DATA_INSTANCE <- "dinst1"
-MASTER_OUT_CSV <- "EWAN_JULY_9__NEW/master_df_hk.csv"
+KEY_OUT_CSV <- "rds_new_key_hki.txt"
 DATA_INSTANCE <- "dinst1"
 
-create_masterdf<-"create_masterdf_function_pos_neg.R"
+MASTER_OUT_CSV <- paste0("master_df_",
+                         EXPERIMENT_NAME,
+                         "_",
+                         DATA_INSTANCE,
+                         ".csv")
+
+create_masterdf<-"fn_create_masterdf_function_pos_neg.R"
 source(create_masterdf)
 master_df<- create_masterdf(vars=c("econ","glob","loc"),
                             coef_vals=c(-1,0,1),
                             exp_name= EXPERIMENT_NAME,
                             data_inst= DATA_INSTANCE)
 
+readr::write_csv(master_df,MASTER_OUT_CSV)
+
+
 #source mdoel_comparison functions, which include function for model fit filename
-model_comparison_functions<-"EWAN_JULY_9__NEW/model_comparison.R"
+model_comparison_functions<-"fn_model_comparison_functions.R"
 source(model_comparison_functions)
 
 master_df$new_name<-model_fit_filename(master_df)
+
+master_tibble<-tibble::as_tibble(master_df)
+
 #create previous names based on legacy code 
 
 mod_name<-
@@ -30,9 +44,10 @@ mod_name<-
     master_df$m_pos_vars =="Econ"~"econ",
     master_df$m_pos_vars =="Glob"~"glob",
     master_df$m_pos_vars =="Loc"~"loc",
-    master_df$m_pos_vars ==""~ "none")
+    master_df$m_pos_vars =="NA"~ "none")
 
-master_df$old_name<-paste0("econ_",master_df$d_coef_econ,
+master_df$old_name<-paste0(
+                          "econ_",master_df$d_coef_econ,
                           "_loc_",master_df$d_coef_loc,
                           "_glob_",master_df$d_coef_glob,
                           "_mod_",mod_name,".rds")
@@ -40,9 +55,9 @@ rm(mod_name)
 
 rds_name_key<-as.data.frame(cbind(master_df$old_name,master_df$new_name))
 
-readr::write_tsv(rds_name_key,"rds_name_key.txt", col_names = FALSE)
+readr::write_tsv(rds_name_key,KEY_OUT_CSV, col_names = FALSE)
 
-#navigate to the old file folder and use the key file
-#while read line; do eval mv $line; done < old_new_names.txt
+#excecute the follolwing in the terminal:
+#while read line; do eval mv $line; done < hindi/hindi/model_fits/rds_name_key.txt
 
 
