7b3ae77468f247338346ac351870f2f95009c53b
ameliakimball <amelia.kimball@gmail.com>
2019-09-27 15:24:33 +0200

    splits brms model running into separate script can be called from command line

---
+++ model_scripts/correct_model_checking/run_brms_mods.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+##########################
+##### brms #####
+##########################
+ARGS <- commandArgs(TRUE)
+
+EXPERIMENT <- ARGS[1]# "hindi""fake_grid"#
+DATA_INST <- ARGS[2] #"dinst5""dinst1" #
+
+corr_master<-readr::read_csv(paste0("corr_master_",
+                                    EXPERIMENT,
+                                    "_",
+                                    DATA_INST,
+                                    ".csv"))
+
+EXP_DATA_COMB <- paste0(EXPERIMENT,"_",DATA_INST)
+
+
+library(brms)
+model_fns <- "../fn_model_comparison_functions.R"
+source(model_fns)
+
+brms_mods <- vector(mode = "list")
+
+for (i in c(1:nrow(corr_master))){
+  out_file = paste0("correct_model_rds/",
+                    model_fit_filename(corr_master[i,]))
+  
+  brms_data <- readr::read_csv(paste0("../sampled_data/",corr_master$csv_filename[i]))
+  
+  brms_mods[[i]] = brms::brm(
+    response_var ~ Econ + Glob + Loc + (1|subject) + (1|trial),
+    data = brms_data,
+    family = 'bernoulli',
+    prior = brms::set_prior('normal(0, 10)'),
+    iter = 2000,
+    chains = 4,
+    cores = 4
+  ) 
+  saveRDS(brms_mods[[i]],file=out_file)
+}
+
+
+saved_brms_mod_list <-saveRDS(brms_mods, paste0("brms_mods_list_",EXP_DATA_COMB,".rds"))
+
