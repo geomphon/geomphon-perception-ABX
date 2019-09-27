1c1fa19351cb9f6a188cee5431fd0bc8adc931a5
ameliakimball <amelia.kimball@gmail.com>
2019-09-27 17:40:25 +0200

    adds brms measures to final corr masters

--- model_scripts/correct_model_checking/build_corr_masters.R
+++ model_scripts/correct_model_checking/build_corr_masters.R
@@ -3,8 +3,18 @@
 `%>%` <- magrittr::`%>%`
 
 # This script must be run for each data instance 
+#NB fake grid does not have acoustic distance or item 
 
-EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
+model_comps<-"../fn_model_comparison_functions.R"
+source(model_comps)
+#nb these functions must come first because they also have experiment settigns,
+#and willoverwrite below variables if come after
+
+EXPERIMENT <- "hindi"
+DATA_INST <- "dinst2"
+
+EXP_DATA_COMB <- paste0(EXPERIMENT,"_",DATA_INST)
+
 
 ######
 #glm 
@@ -20,14 +30,14 @@ EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
       corr_master$Econ_coef_glm <-NA
       corr_master$Glob_coef_glm <-NA
       corr_master$Loc_coef_glm <-NA
-      #corr_master$ac_dist_coef_glm <-NA
-     # corr_master$acoustic_distance <- -.1784 #value of acoustic distance in sampled data
+      corr_master$ac_dist_coef_glm <-NA
+      corr_master$acoustic_distance <- -.1784 #value of acoustic distance in sampled data
       
       
       for (i in 1:nrow(corr_master)) {
         data_list[[i]] <- readr::read_csv(paste0("../sampled_data/",corr_master$csv_filename[i]))
 
-        mod_list[[i]] <- glm(response_var~Econ+Glob+Loc,#+acoustic_distance,
+        mod_list[[i]] <- glm(response_var~Econ+Glob+Loc+acoustic_distance,
                              family=binomial(),
                              data = data_list[[i]])
 
@@ -36,64 +46,78 @@ EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
          corr_master$Econ_coef_glm[i]<-sum_list[[i]]$coefficients[2] #Econ
          corr_master$Glob_coef_glm[i]<-sum_list[[i]]$coefficients[3] #glob
          corr_master$Loc_coef_glm[i]<-sum_list[[i]]$coefficients[4] #Loc
-        # corr_master$ac_dist_coef_glm[i]<-sum_list[[i]]$coefficients[5] #acoustic_distance
+         corr_master$ac_dist_coef_glm[i]<-sum_list[[i]]$coefficients[5] #acoustic_distance
       }
       
       
       corr_master<- 
           dplyr::mutate(corr_master,Econ_glm_diff = Econ_coef_glm-d_coef_econ) %>%
           dplyr::mutate(.,Glob_glm_diff = Glob_coef_glm-d_coef_glob) %>%
-          dplyr::mutate(.,Loc_glm_diff = Loc_coef_glm-d_coef_loc)# %>%
-         # dplyr::mutate(.,ac_dist_diff = ac_dist_coef_glm-acoustic_distance)
+          dplyr::mutate(.,Loc_glm_diff = Loc_coef_glm-d_coef_loc) %>%
+          dplyr::mutate(.,ac_dist_diff = ac_dist_coef_glm-acoustic_distance)
       
-    
-      readr::write_csv(corr_master, paste0("corr_master_",EXP_DATA_COMB,".csv"))
       
       
 ###########
 #ASSUMING AT THIS POINT THAT BRMS HAVE BEEN RUN USING run_brms_mods.R
       
-      brms_fixef <-vector(mode = "list")
       
-      corr_master$intercept_brms_coef <- NA
-      corr_master$Econ_brms_coef <-NA
-      corr_master$Glob_brms_coef <-NA
-      corr_master$Loc_brms_coef <-NA
-     # corr_master$ac_dist_brms_coef <-NA
+brms_fixef <- vector(mode = "list")
+brms_mods  <- vector(mode = "list")
+econ_post  <- vector(mode="list")
+glob_post  <- vector(mode="list")
+loc_post   <- vector(mode="list")
+beta_post  <- vector(mode="list")
       
-      corr_master$intercept_brms_rhat <- NA
-      corr_master$Econ_brms_rhat <-NA
-      corr_master$Glob_brms_rhat <-NA
-      corr_master$Loc_brms_rhat <-NA
-      #corr_master$ac_dist_brms_rhat <-NA
-    
-      #corr_master$loo <-NA
+corr_master$intercept_brms_coef <- NA
+corr_master$Econ_brms_coef      <-NA
+corr_master$Glob_brms_coef      <-NA
+corr_master$Loc_brms_coef       <-NA
+corr_master$ac_dist_brms_coef   <-NA
       
-      
-      
+corr_master$intercept_brms_rhat <-NA
+corr_master$Econ_brms_rhat      <-NA
+corr_master$Glob_brms_rhat      <-NA
+corr_master$Loc_brms_rhat       <-NA
+corr_master$ac_dist_brms_rhat   <-NA
+
+
+
 for (i in 1:nrow(corr_master)){
-  
+    brms_mods[[i]] <- readRDS(paste0("correct_model_rds/",
+                                     EXPERIMENT,
+                                     "/",
+                                     model_fit_filename(corr_master[i,])))
+    
     brms_fixef[[i]] <- brms::fixef(brms_mods[[i]])
 
     corr_master$intercept_brms_coef[i]  <- brms_fixef[[i]][1,1] 
     corr_master$Econ_brms_coef[i]       <- brms_fixef[[i]][2,1] 
     corr_master$Glob_brms_coef[i]       <- brms_fixef[[i]][3,1] 
     corr_master$Loc_brms_coef[i]        <- brms_fixef[[i]][4,1] 
-    #corr_master$ac_dist_brms_coef[i]    <- brms_fixef[[i]][5,1] 
+    corr_master$ac_dist_brms_coef[i]    <- brms_fixef[[i]][5,1] 
     
     corr_master$intercept_brms_rhat[i] <-   rhat(brms_mods[[i]])[[1]]
     corr_master$Econ_brms_rhat[i]      <-   rhat(brms_mods[[i]])[[2]]
     corr_master$Glob_brms_rhat[i]      <-   rhat(brms_mods[[i]])[[3]]
     corr_master$Loc_brms_rhat[i]       <-   rhat(brms_mods[[i]])[[4]]
-   # corr_master$ac_dist_brms_rhat[i]   <-   rhat(brms_mods[[i]])[[5]]
+    corr_master$ac_dist_brms_rhat[i]   <-   rhat(brms_mods[[i]])[[5]]
     
-    }
+    beta_post[[i]] <- brms::posterior_samples(brms_mods[i], "^b")
+    
+    econ_post[[i]] <- beta_post[[i]][["b_Econ"]]
+    glob_post[[i]] <- beta_post[[i]][["b_Glob"]]
+    loc_post[[i]]  <- beta_post[[i]][["b_Loc"]]
+    
+    corr_master$econ_sd[i] <- sd(econ_post[[i]])
+    corr_master$glob_sd[i] <- sd(glob_post[[i]])
+    corr_master$loc_sd[i]  <- sd(glob_post[[i]])
       
+    }
+    
 
 readr::write_csv(corr_master, paste0("final_corr_master_",EXP_DATA_COMB,".csv"))
 
 
-
-      
 
 
