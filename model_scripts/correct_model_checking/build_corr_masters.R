70fddad797c1d7c811a6663c5c76cf9097adc0e6
ameliakimball <amelia.kimball@gmail.com>
2019-09-27 16:19:54 +0200

    change coeff_checks to corr_master builder-- pulls out part of pipeline that extracts summary stats

--- model_scripts/correct_model_checking/build_corr_masters.R
+++ model_scripts/correct_model_checking/build_corr_masters.R
@@ -1,16 +1,14 @@
-### Correct model coeff checking 
-
+### build full corr_masters 
 
 `%>%` <- magrittr::`%>%`
 
-EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
+# This script must be run for each data instance 
 
+EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
 
 ######
 #glm 
 ######
-
-
       master_df <-readr::read_csv(paste0("../master_df_",EXP_DATA_COMB,".csv"))
       
       corr_master <- subset(master_df, master_df$is_correct_model==TRUE)
@@ -52,48 +50,8 @@ EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
       readr::write_csv(corr_master, paste0("corr_master_",EXP_DATA_COMB,".csv"))
       
       
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-##########################
-##### brms #####
-##########################
-      
-library(brms)
-model_fns <- "../fn_model_comparison_functions.R"
-source(model_fns)
-      
-brms_mods <- vector(mode = "list")
-
-      
-for (i in c(1:nrow(corr_master))){
-        out_file = paste0("correct_model_rds/",
-                          model_fit_filename(corr_master[i,]))
-        #+acoustic_distance
-        brms_mods[[i]] = brms::brm(
-          response_var ~ Econ + Glob + Loc + (1|subject) + (1|item),
-          data = data_list[[i]],
-          family = 'bernoulli',
-          prior = brms::set_prior('normal(0, 10)'),
-          iter = 2000,
-          chains = 4,
-          cores = 4
-            ) 
-          saveRDS(brms_mods[[i]],file=out_file)
-      }
-      
-      
-      saved_brms_mod_list <-saveRDS(brms_mods, paste0("brms_mods_list_",EXP_DATA_COMB,".rds"))
-      
-    
+###########
+#ASSUMING AT THIS POINT THAT BRMS HAVE BEEN RUN USING run_brms_mods.R
       
       brms_fixef <-vector(mode = "list")
       
@@ -128,26 +86,12 @@ for (i in 1:nrow(corr_master)){
     corr_master$Glob_brms_rhat[i]      <-   rhat(brms_mods[[i]])[[3]]
     corr_master$Loc_brms_rhat[i]       <-   rhat(brms_mods[[i]])[[4]]
    # corr_master$ac_dist_brms_rhat[i]   <-   rhat(brms_mods[[i]])[[5]]
-
-    #corr_master$loo[i]   <-  loo::loo(brms_mods[[i]])
     
     }
       
 
-readr::write_csv(corr_master, paste0("corr_master_",EXP_DATA_COMB,".csv"))
+readr::write_csv(corr_master, paste0("final_corr_master_",EXP_DATA_COMB,".csv"))
 
-#       
-# 
-# 
-# vec1<-c(1:5)
-# vec2<-c(1:5)      
-# vec3<-c(1:5)      
-# 
-# design <- expand.grid(vec1,vec2,vec3)      
-# 
-# colnames(design) <-c("Econ","Glob","Loc")
-# 
-# readr::write_csv(design, "exp_designs/fake_grid.csv")
 
 
       
