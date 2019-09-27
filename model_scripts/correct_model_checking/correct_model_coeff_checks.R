8c0e78b07f7ce7692bad4e7cbd76b80b32fa9b9b
ameliakimball <amelia.kimball@gmail.com>
2019-09-27 14:24:58 +0200

    adds sd calculations and the posterior draws to full corr_master

--- model_scripts/correct_model_checking/correct_model_coeff_checks.R
+++ model_scripts/correct_model_checking/correct_model_coeff_checks.R
@@ -3,7 +3,7 @@
 
 `%>%` <- magrittr::`%>%`
 
-EXP_DATA_COMB <- "hindi_dinst6" # e.g. hindi_dinst1",
+EXP_DATA_COMB <- "fake_grid_dinst1" # e.g. hindi_dinst1",
 
 
 ######
@@ -22,14 +22,14 @@ EXP_DATA_COMB <- "hindi_dinst6" # e.g. hindi_dinst1",
       corr_master$Econ_coef_glm <-NA
       corr_master$Glob_coef_glm <-NA
       corr_master$Loc_coef_glm <-NA
-      corr_master$ac_dist_coef_glm <-NA
-      corr_master$acoustic_distance <- -.1784 #value of acoustic distance in sampled data
+      #corr_master$ac_dist_coef_glm <-NA
+     # corr_master$acoustic_distance <- -.1784 #value of acoustic distance in sampled data
       
       
       for (i in 1:nrow(corr_master)) {
         data_list[[i]] <- readr::read_csv(paste0("../sampled_data/",corr_master$csv_filename[i]))
 
-        mod_list[[i]] <- glm(response_var~Econ+Glob+Loc, #acoustic_distance,
+        mod_list[[i]] <- glm(response_var~Econ+Glob+Loc,#+acoustic_distance,
                              family=binomial(),
                              data = data_list[[i]])
 
@@ -38,15 +38,15 @@ EXP_DATA_COMB <- "hindi_dinst6" # e.g. hindi_dinst1",
          corr_master$Econ_coef_glm[i]<-sum_list[[i]]$coefficients[2] #Econ
          corr_master$Glob_coef_glm[i]<-sum_list[[i]]$coefficients[3] #glob
          corr_master$Loc_coef_glm[i]<-sum_list[[i]]$coefficients[4] #Loc
-         corr_master$ac_dist_coef_glm[i]<-sum_list[[i]]$coefficients[5] #acoustic_distance
+        # corr_master$ac_dist_coef_glm[i]<-sum_list[[i]]$coefficients[5] #acoustic_distance
       }
       
       
       corr_master<- 
           dplyr::mutate(corr_master,Econ_glm_diff = Econ_coef_glm-d_coef_econ) %>%
           dplyr::mutate(.,Glob_glm_diff = Glob_coef_glm-d_coef_glob) %>%
-          dplyr::mutate(.,Loc_glm_diff = Loc_coef_glm-d_coef_loc) %>%
-          dplyr::mutate(.,ac_dist_diff = ac_dist_coef_glm-acoustic_distance)
+          dplyr::mutate(.,Loc_glm_diff = Loc_coef_glm-d_coef_loc)# %>%
+         # dplyr::mutate(.,ac_dist_diff = ac_dist_coef_glm-acoustic_distance)
       
     
       readr::write_csv(corr_master, paste0("corr_master_",EXP_DATA_COMB,".csv"))
@@ -67,20 +67,19 @@ EXP_DATA_COMB <- "hindi_dinst6" # e.g. hindi_dinst1",
 ##### brms #####
 ##########################
       
-      
-      library(brms)
-model_fns <- "fn_model_comparison_functions.R"
+library(brms)
+model_fns <- "../fn_model_comparison_functions.R"
 source(model_fns)
       
 brms_mods <- vector(mode = "list")
 
       
 for (i in c(1:nrow(corr_master))){
-        out_file = paste0("correct_model_checking/correct_model_rds/",
+        out_file = paste0("correct_model_rds/",
                           model_fit_filename(corr_master[i,]))
-        
-        brms_mods[[i]] = brm(
-          response_var ~ Econ+Glob+Loc+acoustic_distance + (1|subject) + (1|item),
+        #+acoustic_distance
+        brms_mods[[i]] = brms::brm(
+          response_var ~ Econ + Glob + Loc + (1|subject) + (1|item),
           data = data_list[[i]],
           family = 'bernoulli',
           prior = brms::set_prior('normal(0, 10)'),
@@ -102,13 +101,13 @@ for (i in c(1:nrow(corr_master))){
       corr_master$Econ_brms_coef <-NA
       corr_master$Glob_brms_coef <-NA
       corr_master$Loc_brms_coef <-NA
-      corr_master$ac_dist_brms_coef <-NA
+     # corr_master$ac_dist_brms_coef <-NA
       
       corr_master$intercept_brms_rhat <- NA
       corr_master$Econ_brms_rhat <-NA
       corr_master$Glob_brms_rhat <-NA
       corr_master$Loc_brms_rhat <-NA
-      corr_master$ac_dist_brms_rhat <-NA
+      #corr_master$ac_dist_brms_rhat <-NA
     
       #corr_master$loo <-NA
       
@@ -122,13 +121,13 @@ for (i in 1:nrow(corr_master)){
     corr_master$Econ_brms_coef[i]       <- brms_fixef[[i]][2,1] 
     corr_master$Glob_brms_coef[i]       <- brms_fixef[[i]][3,1] 
     corr_master$Loc_brms_coef[i]        <- brms_fixef[[i]][4,1] 
-    corr_master$ac_dist_brms_coef[i]    <- brms_fixef[[i]][5,1] 
+    #corr_master$ac_dist_brms_coef[i]    <- brms_fixef[[i]][5,1] 
     
     corr_master$intercept_brms_rhat[i] <-   rhat(brms_mods[[i]])[[1]]
     corr_master$Econ_brms_rhat[i]      <-   rhat(brms_mods[[i]])[[2]]
     corr_master$Glob_brms_rhat[i]      <-   rhat(brms_mods[[i]])[[3]]
     corr_master$Loc_brms_rhat[i]       <-   rhat(brms_mods[[i]])[[4]]
-    corr_master$ac_dist_brms_rhat[i]   <-   rhat(brms_mods[[i]])[[5]]
+   # corr_master$ac_dist_brms_rhat[i]   <-   rhat(brms_mods[[i]])[[5]]
 
     #corr_master$loo[i]   <-  loo::loo(brms_mods[[i]])
     
@@ -136,18 +135,19 @@ for (i in 1:nrow(corr_master)){
       
 
 readr::write_csv(corr_master, paste0("corr_master_",EXP_DATA_COMB,".csv"))
-      
 
-
-vec1<-c(1:5)
-vec2<-c(1:5)      
-vec3<-c(1:5)      
-
-design <- expand.grid(vec1,vec2,vec3)      
-
-colnames(design) <-c("Econ","Glob","Loc")
-
-readr::write_csv(design, "exp_designs/fake_grid.csv")
+#       
+# 
+# 
+# vec1<-c(1:5)
+# vec2<-c(1:5)      
+# vec3<-c(1:5)      
+# 
+# design <- expand.grid(vec1,vec2,vec3)      
+# 
+# colnames(design) <-c("Econ","Glob","Loc")
+# 
+# readr::write_csv(design, "exp_designs/fake_grid.csv")
 
 
       
--- model_scripts/correct_model_checking/correct_models_vizualizations.R
+++ model_scripts/correct_model_checking/correct_models_vizualizations.R
@@ -22,27 +22,29 @@ hindi_for_hists<-dplyr::bind_rows(hindi_dinst1,
                                   hindi_dinst6)
 
 
-
 Econ_hist <- ggplot2::ggplot(hindi_for_hists, aes(x = Econ_glm_diff)) +
-  geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Econ coef differenece,hindi data,calculated by glm, by number of subjects")+
-  facet_grid(numsubjs ~ .)
+             ggplot2::geom_histogram(binwidth = .1,
+                                     position="identity",
+                                     alpha = 0.4)+
+             ggplot2::scale_x_continuous(limits = c(-10,10))+
+             ggplot2::ggtitle("Econ coef differenece,hindi data,
+                              calculated by glm, by number of subjects")+
+             ggplot2::facet_grid(numsubjs ~ .)
 Econ_hist
 
 
 Glob_hist <- ggplot2::ggplot(hindi_for_hists, aes(x = Glob_glm_diff)) +
-  geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Glob coef differenece, hindi data, calculated by glm, by number of subjects")+
-  facet_grid(numsubjs ~ .)
+             ggplot2::geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
+             ggplot2::scale_x_continuous(limits = c(-10,10))+
+             ggplot2::ggtitle("Glob coef differenece, hindi data, calculated by glm, by number of subjects")+
+             ggplot2::facet_grid(numsubjs ~ .)
 Glob_hist
 
 Loc_hist <- ggplot2::ggplot(hindi_for_hists, aes(x = Loc_glm_diff)) +
-  geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Loc coef differenece, hindi data,calculated by glm, by number of subjects")+
-  facet_grid(numsubjs ~ .)
+            ggplot2::geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
+            ggplot2::scale_x_continuous(limits = c(-10,10))+
+            ggplot2::ggtitle("Loc coef differenece, hindi data,calculated by glm, by number of subjects")+
+            ggplot2::facet_grid(numsubjs ~ .)
 Loc_hist
 
 
@@ -65,92 +67,72 @@ fake_grid_for_hists<-dplyr::bind_rows(fake_grid_dinst1,
                                       fake_grid_dinst5)
 
 
-
-Econ_hist <- ggplot2::ggplot(fake_grid_for_hists, aes(x = Econ_glm_diff)) +
-  geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Econ coef differenece,fake grid data,calculated by glm, by number of subjects")+
-  facet_grid(numsubjs ~ .)
+library(ggplot2)
+Econ_hist <-ggplot2::ggplot(fake_grid_for_hists, aes(x = Econ_glm_diff)) +
+            ggplot2::geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
+            ggplot2::scale_x_continuous(limits = c(-10,10))+
+            ggplot2::ggtitle("Econ coef differenece,fake grid data,calculated by glm, by number of subjects")+
+            ggplot2::facet_grid(numsubjs ~ .)
 Econ_hist
 
-
-Glob_hist <- ggplot2::ggplot(fake_grid_for_hists, aes(x = Glob_glm_diff)) +
-  geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Glob coef differenece, fake grid data, calculated by glm, by number of subjects")+
-  facet_grid(numsubjs ~ .)
+Glob_hist <-ggplot2::ggplot(fake_grid_for_hists, aes(x = Glob_glm_diff)) +
+            ggplot2::geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
+            ggplot2::scale_x_continuous(limits = c(-10,10))+
+            ggplot2::ggtitle("Glob coef differenece, fake grid data, calculated by glm, by number of subjects")+
+            ggplot2::facet_grid(numsubjs ~ .)
 Glob_hist
 
-Loc_hist <- ggplot2::ggplot(fake_grid_for_hists, aes(x = Loc_glm_diff)) +
-  geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Loc coef differenece, fake grid data,calculated by glm, by number of subjects")+
-  facet_grid(numsubjs ~ .)
+Loc_hist <- ggplot2::ggplot(fake_grid_for_hists, ggplot2::aes(x = Loc_glm_diff)) +
+            ggplot2::geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
+            ggplot2::scale_x_continuous(limits = c(-10,10))+
+            ggplot2::ggtitle("Loc coef differenece, fake grid data,calculated by glm, by number of subjects")+
+            ggplot2::facet_grid(numsubjs ~ .)
 Loc_hist
 
 
+#inspect the loc scores- are they wonky? 
+
+# if we take out loc, can we extract the coeffs better / with fewer subjects? 
 
 
 
+posteriors <- vector(mode = "list")
+
+sample_rds <- readRDS(paste0("correct_model_rds/",model_fit_filename(corr_master[1,])))
 
 
 
+rds_list <- vector(mode="list")
+econ_post <-vector(mode="list")
+glob_post <-vector(mode="list")
+loc_post <-vector(mode="list")
+beta_post<-vector(mode="list")
+
+
+combined_corr <- dplyr::bind_rows( )
+
+
+for (i in c(1:5)) {
+  rds_list[[i]] <- readRDS(paste0("correct_model_rds/",model_fit_filename(corr_master[i,])))
+  beta_post[[i]]<-brms::posterior_samples(rds_list[i], "^b")
+  econ_post[[i]]<- beta_post[[i]][["b_Econ"]]
+  glob_post[[i]]<- beta_post[[i]][["b_Glob"]]
+  loc_post[[i]] <- beta_post[[i]][["b_Loc"]]
+  
+  corr_master$econ_sd[i] <- sd(econ_post[[i]])
+  corr_master$glob_sd[i] <- sd(glob_post[[i]])
+  corr_master$loc_sd[i] <- sd(glob_post[[i]])
+  
+}
 
 
 
-
-
-
-
-
-par(mfrow=c(3,1))
-
-hindi_dinst1<- 
-  dplyr::mutate(hindi_dinst1,Econ_brms_diff = Econ_brms_coef-d_coef_econ) %>%
-  dplyr::mutate(.,Glob_brms_diff = Glob_brms_coef-d_coef_glob) %>%
-  dplyr::mutate(.,Loc_brms_diff = Loc_brms_coef-d_coef_loc)
-
-
-
-Glob_hist <- ggplot2::ggplot(all_hindi, aes(x = Glob_brms_diff, fill = d_id)) +
-  geom_histogram()+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Glob_Brms_difference")
-Glob_hist
-
-
-Loc_hist <- ggplot2::ggplot(all_hindi, aes(x = Loc_brms_diff, fill = d_id)) +
-  geom_histogram()+
-  scale_x_continuous(limits = c(-6,6))+
-  ggtitle("Loc_Brms_difference")
-Loc_hist
-
-
-
-
-
-
-
-hk_dinst2<-readr::read_csv("corr_master_hk_dinst2.csv")
-
-
-
-hk_dinst2<- 
-  dplyr::mutate(hk_dinst2,Econ_brms_diff = Econ_brms_coef-d_coef_econ) %>%
-  dplyr::mutate(.,Glob_brms_diff = Glob_brms_coef-d_coef_glob) %>%
-  dplyr::mutate(.,Loc_brms_diff = Loc_brms_coef-d_coef_loc)
-
-hist(hk_dinst2$Econ_glm_diff, main= "hk_dinst2_glm_Econ_diff", xlim =c(-10,10))
-hist(hk_dinst2$Glob_glm_diff,main= "hk_dinst2_glm_Glob_diff", xlim =c(-10,10))
-hist(hk_dinst2$Loc_glm_diff,main= "hhk_dinst2_glm_Loc_diff", xlim =c(-10,10))
-
-hist(hk_dinst2$Econ_brms_diff, main= "hk_dinst2_brms_Econ_diff", xlim =c(-10,10))
-hist(hk_dinst2$Glob_brms_diff,main= "hk_dinst2_brms_Glob_diff", xlim =c(-10,10))
-hist(hk_dinst2$Loc_brms_diff,main= "hk_dinst2_brms_Loc_diff", xlim =c(-10,10))
-
-
-
-
+Econ_hist <-ggplot2::ggplot(corr_master, aes(x = Econ_glm_diff)) +
+  ggplot2::geom_histogram(binwidth = .1,position="identity", alpha = 0.4)+
+  ggplot2::scale_x_continuous(limits = c(-10,10))+
+  ggplot2::ggtitle("Econ coef differenece,fake grid data,calculated by glm, by number of subjects")+
+  ggplot2::facet_grid(numsubjs ~ .)
+Econ_hist
 
 
 
